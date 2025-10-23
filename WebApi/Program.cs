5d97ca9af78a7c2028eb8908c7cf29cc1d19fff6
frell <frell@FERCHO_10BD>
2025-10-23 14:27:03 -0500

    quitando el punto que arruino el despliegue

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(options =>
             policy
                 .AllowAnyOrigin()    
                 .AllowAnyHeader() 
-                .AllowAnyMethod();  .
+                .AllowAnyMethod();  
         });
 });
 
